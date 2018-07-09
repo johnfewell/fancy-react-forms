61919cf4d16cac55f0d7c4224015a56f1231ad93
meq <fewell@gmail.com>
2018-07-09 19:31:27 -0400

    FormsIndex working

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import './semantic/dist/semantic.min.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import FormsIndex from './components/forms_index'
+
+
 ReactDOM.render(
   <BrowserRouter>
     <div>
-
+      <Route path="/" component={FormsIndex} />
     </div>
   </BrowserRouter>
 ,  document.getElementById('root'), // eslint-disable-line no-undef
