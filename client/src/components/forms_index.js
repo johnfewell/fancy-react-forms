9301b6c8c81f83bc653823b2fddf85d1e0e086b2
meq <fewell@gmail.com>
2018-07-09 19:26:26 -0400

    Added the form index component

---
+++ client/src/components/forms_index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FormsIndex extends Component {
+  render () {
+    return (
+      <div>
+        Forms Index
+      </div>
+    );
+  }
+}
+
+
+export default FormsIndex
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import './semantic/dist/semantic.min.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root'), // eslint-disable-line no-undef
+  <BrowserRouter>
+  <div>
+
+    </div>
+  </BrowserRouter>
+,  document.getElementById('root'), // eslint-disable-line no-undef
 );
