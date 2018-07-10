75cae0d2096290b6ebe1987400696ae56c8078ec
meq <fewell@gmail.com>
2018-07-10 17:45:00 -0400

    Add semantic ui and styling to index page

--- client/src/components/forms_index.js
+++ client/src/components/forms_index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchForms } from '../actions';
+import { Card, Header, Container, Grid } from 'semantic-ui-react';
 
 class FormsIndex extends Component {
   componentDidMount() {
@@ -10,22 +11,27 @@ class FormsIndex extends Component {
 
   renderForms(){
     return _.map(this.props.forms.forms, form => {
+      const formUrl = `forms/${form.id}`
       return (
-        <li className="list-group-item" key={form.id}>
-          {form.name}
-        </li>
+          <Card
+            href={formUrl}
+            key={form.id}
+            header={form.name}
+            meta='0 Responses'
+            description='Duis convallis varius tellus id gravida. Vestibulum pulvinar lacus hendrerit neque ullamcorper sagittis. Nullam quis lectus et ligula ullamcorper iaculis sit amet et purus. Morbi consectetur dui sit amet massa sollicitudin, et consectetur est malesuada. Pellentesque ac malesuada mauris.'
+          />
       )
     });
   }
 
   render () {
     return (
-      <div>
-        <h3>Forms</h3>
-        <ul className="list-group">
+      <Container>
+        <Header as='h1'>Forms</Header>
+        <Card.Group itemsPerRow={4}>
           {this.renderForms()}
-        </ul>
-      </div>
+        </Card.Group>
+      </Container>
     );
   }
 }
