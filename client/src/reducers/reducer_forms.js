23c3fe02ccd645f9d494cc40d358aa873e06cd30
meq <fewell@gmail.com>
2018-07-11 14:47:03 -0400

    Refactored reducer to have the id as the key for forms state

--- client/src/reducers/reducer_forms.js
+++ client/src/reducers/reducer_forms.js
@@ -4,12 +4,12 @@ export default function(state = {}, action) {
   switch (action.type) {
     case 'FETCH_FORMS':
       return state
+    case 'FETCH_FORM':
+      return {...state, }
     case 'FETCH_FORMS_ERROR':
       return { ...state, error: action.payload }
     case 'RECEIVED_FORMS':
-      return {
-        forms_state: action.payload
-      }
+      return {forms_state: _.mapKeys(action.payload, 'id')}
     default:
       return state;
   }
