295e8a118e56d60cec249686b3c4fd778e6f231d
meq <fewell@gmail.com>
2018-07-11 14:41:47 -0400

    Added fetchPost action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -15,6 +15,24 @@ function fetchForms () {
   }
 }
 
+function fetchForm(id) {
+  return (dispatch) => {
+    dispatch({type: 'FETCH_FORM'})
+    return fetch(`api/forms/${id}`)
+      .then(response => {
+        response.json()
+        .then(json => {
+
+          dispatch({type: 'RECEIVED_FORM', payload: json})
+        })
+      })
+      .catch((err) => {
+        dispatch({type: 'FETCH_FORM_ERROR', payload: err})
+      })
+  }
+}
+
 export {
-  fetchForms
+  fetchForms,
+  fetchForm
 }
