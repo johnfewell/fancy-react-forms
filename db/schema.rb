3d70c267aab63780ac2c6c96e300b77881493621
meq <fewell@gmail.com>
2018-08-09 15:58:17 -0400

    switched to postgres from sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20181216234200) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
     t.text "content"
